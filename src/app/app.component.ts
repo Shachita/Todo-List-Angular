54f24a0fa020514ed675ccf8586aa3af2781d1f0
Shachita <shachitajains@gmail.com>
2020-05-06 17:04:36 +0530

    3 files modified, 1 added(Event handling done)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
       Welcome to {{ title }}!
     </h1>
 
-    <app-input-button-unit></app-input-button-unit>
+    <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
 
     <ul>
       <li *ngFor="let todoItem of todoList">
@@ -18,8 +19,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
-  todoList = [
+  title = 'App Component';
+  todoList : TodoItem[] =  [
     {title: 'install NodeJS'},
     {title: 'install Angular CLI'},
     {title: 'create new app'},
@@ -27,4 +28,8 @@ export class AppComponent {
     {title: 'develop app'},
     {title: 'deploy app'},
   ];
+  addItem(title: string)
+  {
+    this.todoList.push({title});
+  }
 }
